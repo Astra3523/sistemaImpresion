81a094cc2712fb2121b51c08f85f16a6023a26aa
Saul <overcipress103@gmail.com>
2025-10-15 18:39:32 -0600

    Primer Commit

---
+++ src/src/SistemaImpresion.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package src;
+
+/**
+ *
+ * @author Astra
+ */
+public class SistemaImpresion {
+    public static void main(String args[]){
+        
+    }
+}
